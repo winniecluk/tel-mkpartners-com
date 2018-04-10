554abe557acc99b932b0d668c8b5e56b5732593c
Winnie Luk <wluk@mkpartners.com>
2018-04-10 16:12:28 -0700

    create function that records calls in firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,15 +9,15 @@ import * as builder from 'xmlbuilder';
 admin.initializeApp();
 
 // main object that contains object and keys
+// const selfUrl: string = 'https://us-central1-tel-mkpartners-com.cloudfunctions.net/receiveCall';
 const domainName: string = '@mkpartnerswinnie.sip.us1.twilio.com';
 const xmlHeader: string = '<?xml version="1.0" encoding="UTF-8"?>';
 
 // req.query.round currently zero-indexed in twilio interface
 // data structure not decided
-const numberPool: object = [
-    [{sip: `727${domainName}`, number: '12095059520'}]
+const numberPool: any = [
+    [{sip: `727${domainName}`, number: ''}]
     , [{sip: `729${domainName}`}]
-    , []
 ]
 
 // do I really need a map?
@@ -29,56 +29,139 @@ const numberPool: object = [
 // method to update the numbers in sip domain
 
 
+exports.recordCallDetails = functions.https.onRequest((req, res) => {
+    console.log('recording call details');
+    recordEndCall(req);
+
+    // record here
+    // DialCallStatus
+    // CallerState
+    //  CallerZip
+    //
+});
+
+function recordEndCall(req){
+    // const callerNumber = req.body.Caller.substring(1);
+    const date = new Date().toISOString().split('T')[0];
+    admin.firestore().collection('calls').doc(date).collection('calls').doc(req.query.Id)
+    // .collection(callerNumber).doc(req.query.docId)
+    .update({
+        callEndTime: admin.firestore.FieldValue.serverTimestamp(),
+        callStatus: 'Completed call.'
+    })
+    .then(() => {
+        console.log("Document updated!");
+    })
+    .catch(error => {
+        console.error("Error updating document: ", error);
+    });
+}
+
+function recordStartCall(body){
+    const date = new Date().toISOString().split('T')[0];
+    const collectionRef = admin.firestore().collection('calls').doc(date).collection('calls');
+    // .doc(date).collection(callerNumber);
+
+    return collectionRef.add({
+        callStartTime: admin.firestore.FieldValue.serverTimestamp(),
+        callStatus: 'Did not pick up',
+        number: body.From
+    })
+    .then(savedDoc => {
+        console.log("Document written with ID: ", savedDoc.id);
+        return savedDoc.id;
+    })
+    .catch(error => {
+        console.error("Error adding document: ", error);
+        return error;
+    });
+}
 
 exports.receiveCall = functions.https.onRequest((req, res) => {
     let xmlStr = xmlHeader;
-    let xmlBuilder = builder.create('Response');
-    const roundNum = req.query.round;
-
-    if (req.query.round){
-        buildXmlStr(xmlBuilder, parseInt(roundNum), 'There are no available numbers in this pool.').then(result => {
-            xmlStr += result.end();
-            xmlStr = xmlStr.replace('<?xml version="1.0"?>', '');
-            console.log('this is the xml string');
-            console.log(xmlStr);
-            res.send(xmlStr);
-            return 'OK';
-        }).catch(err => {
-            console.log(err);
+    const xmlBuilder = builder.create('Response');
+    // const roundNum = req.query.round;
+    //
+    // if (req.query.round){
+        recordStartCall(req.body)
+        .then(docRefId => {
+            buildXmlStr(xmlBuilder, docRefId, 'There are no available numbers in this pool.').then(result => {
+                xmlStr += result;
+                xmlStr = xmlStr.replace('<?xml version="1.0"?>', '');
+                console.log('this is the xml string');
+                console.log(xmlStr);
+                res.send(xmlStr);
+                return 'OK';
+            }).catch(err => {
+                console.log(err);
+            })
         })
-    }
-    else {
-        buildCustomMessage(xmlBuilder, 'woman', 'Missing query params round.').then(result => {
-            xmlStr += result.end();
-            xmlStr = xmlStr.replace('<?xml version="1.0"?>', '');
-            console.log('this is the xml string');
-            console.log(xmlStr);
-            res.send(xmlStr);
-            return 'OK';
-        }).catch(err => {
-            console.log(err);
-        })
-    }
+        .catch(error => {
+            console.log(error);
+        });
+    // }
+    // else {
+    //     buildCustomMessage(xmlBuilder, 'woman', 'Missing query params round.').then(result => {
+    //         console.log(result);
+    //         xmlStr += result.end();
+    //         xmlStr = xmlStr.replace('<?xml version="1.0"?>', '');
+    //         console.log('this is the xml string');
+    //         console.log(xmlStr);
+    //         res.send(xmlStr);
+    //         return 'OK';
+    //     }).catch(err => {
+    //         console.log(err);
+    //     })
+    // }
 
 });
 
-async function buildXmlStr(xmlBuilder: any, roundNum: number, customMessage: string){
-    if (numberPool[roundNum]
-        && numberPool[roundNum].length > 0){
-        const xmlPromise = new Promise((resolve, reject) => {
-            var dialElement = xmlBuilder.ele('Dial');
-            for (let i = 0; i < numberPool[roundNum].length; i++){
-                dialElement.ele('Sip',numberPool[roundNum][i]['sip']);
-                dialElement.ele('Number',numberPool[roundNum][i]['number']);
+
+async function buildXmlStr(xmlBuilder: any, docRefId: string, customMessage: string){
+    const xmlPromise = new Promise((resolve, reject) => {
+        for (let i = 0; i < numberPool.length; i++){
+            const dialElement = xmlBuilder.ele('Dial');
+            dialElement.att('action', 'https://us-central1-tel-mkpartners-com.cloudfunctions.net/recordCallDetails?docId=' + docRefId);
+            dialElement.att('method', 'POST');
+            dialElement.att('timeout', 20);
+            for (let j = 0; j < numberPool[i].length; j++){
+                if (numberPool[i][j]['number']){
+                    dialElement.ele('Number',numberPool[i][j]['number']);
+                }
+                if (numberPool[i][j]['sip']){
+                    dialElement.ele('Sip',numberPool[i][j]['sip']);
+                }
             }
-            resolve(xmlBuilder);
-        });
-        const result = await xmlPromise;
-        return result;
-    } else {
-        return buildCustomMessage(xmlBuilder, 'woman', customMessage);
-    }
+        }
+        resolve(xmlBuilder.end());
+    });
+    let result = await xmlPromise;
+    return result;
 }
+
+// async function buildXmlStr(xmlBuilder: any, roundNum: number, customMessage: string){
+//     if (numberPool[roundNum]
+//         && numberPool[roundNum].length > 0){
+//         const xmlPromise = new Promise((resolve, reject) => {
+//             let dialElement = xmlBuilder.ele('Dial');
+//             // dialElement.att('action', selfUrl + '?round=' + (roundNum + 1).toString() );
+//             // dialElement.att('method', 'POST');
+//             for (let i = 0; i < numberPool[roundNum].length; i++){
+//                 if (numberPool[roundNum][i]['number']){
+//                     dialElement.ele('Number',numberPool[roundNum][i]['number']);
+//                 }
+//                 if (numberPool[roundNum][i]['sip']){
+//                     dialElement.ele('Sip',numberPool[roundNum][i]['sip']);
+//                 }
+//             }
+//             resolve(xmlBuilder);
+//         });
+//         const result = await xmlPromise;
+//         return result;
+//     } else {
+//         return buildCustomMessage(xmlBuilder, 'woman', customMessage);
+//     }
+// }
 
 function buildCustomMessage(xmlBuilder: any, voice: string, customMessage: string){
     xmlBuilder.ele('Say', {voice: voice}, customMessage)
